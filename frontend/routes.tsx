c22796a3b15d7ab31fa694735caa57ee04167ca4
Kriss Seglins <101052651+krissvaa@users.noreply.github.com>
2024-01-24 20:11:15 +0200

    Client side route protection

--- frontend/routes.tsx
+++ frontend/routes.tsx
@@ -4,11 +4,15 @@ import { createBrowserRouter, RouteObject } from 'react-router-dom';
 import { getFsViews } from './util/routing';
 import viewComponents from 'Frontend/generated/views.js';
 import {serverSideRoutes} from 'Frontend/generated/flow/Flow';
+import {protectRoutes} from "@hilla/react-auth";
 
-const fsRoutes = getFsViews().filter(view => view.clientSide).map(view => {
-    return {path: view.route, element: createElement((viewComponents as any)[view.id]), handle: {title: view.title, rolesAllowed: view.rolesAllowed}};
+const fsRoutes = protectRoutes(getFsViews().filter(view => view.clientSide).map(view => {
+    return {
+      path: view.route, element: createElement((viewComponents as any)[view.id]),
+      handle: {title: view.title, rolesAllowed: view.rolesAllowed },
+    }
   }
-);
+), "/login");
 
 export const routes = [
   {
