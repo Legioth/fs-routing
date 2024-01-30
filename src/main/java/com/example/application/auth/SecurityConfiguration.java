17e1169a790af1187e7a55337fec284f46b73d67
Kriss Seglins <kriss@vaadin.com>
2024-01-30 10:11:15 +0200

    Fix login

--- src/main/java/com/example/application/auth/SecurityConfiguration.java
+++ src/main/java/com/example/application/auth/SecurityConfiguration.java
@@ -5,14 +5,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfiguration extends VaadinWebSecurity {
 
@@ -21,6 +24,12 @@ public class SecurityConfiguration extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf(registry -> {
+            registry.ignoringRequestMatchers(new AntPathRequestMatcher("/connect/**"));
+            registry.ignoringRequestMatchers(new AntPathRequestMatcher("/VAADIN/**"));
+            registry.ignoringRequestMatchers(new AntPathRequestMatcher("/login"));
+            registry.ignoringRequestMatchers(new AntPathRequestMatcher("/logout"));
+        });
         http.authorizeHttpRequests(registry -> {
             registry.requestMatchers(new PublicClientViewRequestMatcher()).permitAll();
         });
